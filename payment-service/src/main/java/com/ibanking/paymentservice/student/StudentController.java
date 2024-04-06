2ca158a6f34344df7189d520365f4f96adc51011
Kelvin Vu <kelvn.developer@gmail.com>
2024-04-06 17:22:12 +0700

    remove unused API

--- payment-service/src/main/java/com/ibanking/paymentservice/student/StudentController.java
+++ payment-service/src/main/java/com/ibanking/paymentservice/student/StudentController.java
@@ -1,6 +1,5 @@
 package com.ibanking.paymentservice.student;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +10,6 @@ import org.springframework.web.bind.annotation.*;
 public class StudentController {
   private final StudentService studentService;
   private final StudentMapper studentMapper;
-
-  @PostMapping
-  public ResponseEntity<?> create(@Valid @RequestBody StudentReqDto dto) {
-    studentService.create(dto);
-    return ResponseEntity.ok(null);
-  }
 
   @GetMapping("/{studentId}")
   public ResponseEntity<?> getById(@PathVariable(name = "studentId") String studentId) {
