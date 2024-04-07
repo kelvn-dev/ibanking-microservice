ce615b7dca7937747c048f2d5654b5a65286877a
Kelvin Vu <kelvn.developer@gmail.com>
2024-04-07 12:05:14 +0700

    update UserReqDto

--- user-service/src/main/java/com/ibanking/userservice/user/UserReqDto.java
+++ user-service/src/main/java/com/ibanking/userservice/user/UserReqDto.java
@@ -1,10 +1,14 @@
 package com.ibanking.userservice.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class UserReqDto {
   @NotBlank private String fullName;
-  @NotBlank private String phone;
+
+  @NotBlank
+  @Pattern(regexp = "[\\d]{10}")
+  private String phone;
 }
